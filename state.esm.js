27798248cd4e559e9f690becb45cd9221f9c7b0c
Cheny Chen <linux.experts111@gmail.com>
2019-12-17 18:25:05 +0800

    add sample

--- state.esm.js
+++ state.esm.js
@@ -1,11 +1,47 @@
 'use strict'
 
 import { EventEmitter } from  'events';
-import { BitwiseCompareBE } from "beson/helper.esm.js";
+import { BitwiseCompareBE } from 'beson/helper.esm.js';
 import { Serialize } from 'beson/serialize.esm.js';
 import { _ContentType, _Version, _Handshake } from './recordProtocolFormat.esm.js';
 
 
+class _StateMachine {
+    constructor() {
+    	this._prev_state = false;
+        this._state = false;
+        this._state_data = null;
+        this._state_map = Object.create(null);
+        this._shared_data = {};
+        Object.defineProperties(this._shared_data, {
+        	push_state: {
+        		value: (...args) => this.push_state(...args),
+        		configurable:false, writable:false, enumerable:true
+        	},
+        	prev_state: { get:() => this._prev_state, configurable:false, enumerable:true },
+        	curr_state: { get:() => this._state, configurable:false, enumerable:true },
+        	state_data: { get:() => this._state_data, configurable:false, enumerable:true }
+        })
+    }
+    register(key, processor) {
+    	this._state_map[key] = processor;
+    }
+    push_state(state_key, state_data) {
+    	this._prev_state = this._state;
+		this._state = state_key;
+		this._state_data = state_data;
+    }
+    get state() { return this._state; }
+    set state(state) { this._state = '' + state; }
+    run(...args) {
+    	const processor = this._state_map[this._state];
+    	if ( !processor ) {
+    		throw new Error( "Undetermined State!" );
+    	}
+    	
+    	return processor.call(this._shared_data, ...args);
+    }
+}
 
 class FiniteStateMachines extends EventEmitter {
     constructor() {
@@ -14,6 +50,41 @@ class FiniteStateMachines extends EventEmitter {
     }
 
 }
+
+FiniteStateMachines.prototype.__HEADER = Object.freeze({ 
+    _Client_Hello: new Uint8Array(
+            [
+                _ContentType.HANDSHAKE,
+                _Version.MAJOR,
+                _Version.MINOR,
+                _Handshake.CLIENT_HELLO
+            ]),
+
+    _Server_ACK: new Uint8Array(
+            [
+                _ContentType.HANDSHAKE,
+                _Version.MAJOR,
+                _Version.MINOR,
+                _Handshake.SERVER_ACK
+            ]),
+
+    _Certificate: new Uint8Array(
+            [
+                _ContentType.HANDSHAKE,
+                _Version.MAJOR,
+                _Version.MINOR,
+                _Handshake.CERTIFICATE   
+            ]),
+    
+    _Change_Cipher_Spec: new Uint8Array(
+            [
+                _ContentType.CHANGE_CIPHER_SPEC,
+                _Version.MAJOR,
+                _Version.MINOR,  
+            ]),
+})
+
+
 
 Object.defineProperties(FiniteStateMachines.prototype, {
     _Client_Hello: {
@@ -77,8 +148,7 @@ Object.defineProperties(FiniteStateMachines.prototype, {
 })
 
 const a = new FiniteStateMachines();
-console.log(
- a);
+console.log(a.__HEADER._Change_Cipher_Spec);
 
 
 export const _Client_Hello = Serialize(new Uint8Array(
---
+++ state3.esm.js
@@ -0,0 +1,101 @@
+/**
+ *	Author: JCloudYu
+ *	Create: 2019/12/17
+**/
+class _StateMachine {
+    constructor() {
+    	this._prev_state = false;
+        this._state = false;
+        this._state_data = null;
+        this._state_map = Object.create(null);
+        this._shared_data = {};
+        Object.defineProperties(this._shared_data, {
+        	push_state: {
+        		value:(...args)=>this.push_state(...args),
+        		configurable:false, writable:false, enumerable:true
+        	},
+        	prev_state: { get:()=>this._prev_state, configurable:false, enumerable:true },
+        	curr_state: { get:()=>this._state, configurable:false, enumerable:true },
+        	state_data: { get:()=>this._state_data, configurable:false, enumerable:true }
+        })
+    }
+    register(key, processor) {
+    	this._state_map[key] = processor;
+    }
+    push_state(state_key, state_data) {
+    	this._prev_state = this._state;
+		this._state = state_key;
+		this._state_data = state_data;
+    }
+    get state() { return this._state; }
+    set state(state) { this._state = '' + state; }
+    run(...args) {
+    	const processor = this._state_map[this._state];
+    	if ( !processor ) {
+    		throw new Error( "Undetermined State!" );
+    	}
+    	
+    	return processor.call(this._shared_data, ...args);
+    }
+}
+​
+​
+​
+​
+export const StateMachine = new Proxy(_StateMachine, {
+	construct: (target, args)=>{
+		const inst = new Proxy(new target(...args), {
+			apply:(target, args)=>{
+				target.run(...args);
+			}
+		});
+		
+		return inst;
+	}
+});
+​
+​
+​
+​
+//console.log(StateMachine);
+const item = new StateMachine();
+//console.log(item);
+​
+​
+item.state = 'initial';
+item.register('initial', function(...args){
+	this._name = "Cloud";
+	console.log("Initial State", this.prev_state, this.state_data, args);
+	this.push_state('a', {a:1,b:2,c:3});
+});
+item.register('a', function(...args){
+	console.log("A State", this._name, this.prev_state, this.state_data, args);
+	this.push_state('a', {a:1,b:2,c:3});
+});
+item.register('b', function(...args) {
+	console.log("B State", this._name, this.prev_state, this.state_data, args);
+	this.push_state('terminate', {g:1,h:2,i:3});
+});
+item.register('terminate', function(...args){
+	console.log("Terminate State", this._name, this.prev_state, this.state_data, args);
+});
+​
+​
+​
+console.log( "run1" );
+item.run('run1', 1, 2, 3);
+​
+console.log( "\nrun2" );
+item.run('run2', 4, 5, 6);
+​
+console.log( "\nrun3" );
+item.run('run3', 7, 8, 9);
+​
+console.log( "\nrun4" );
+item.run('run4', 10, 11, 12);
+​
+console.log( "\nrun5" );
+item.run('run5', 13, 14, 15);
+​
+console.log( "\nrun6" );
+item.run('run6', 16, 17, 18);
