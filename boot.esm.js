ddb2daedd5ca7dbc18c1757b0582499e751bfe97
Cheny Chen <linux.experts111@gmail.com>
2020-03-05 21:12:15 +0800

    update

--- boot.esm.js
+++ boot.esm.js
@@ -1,21 +1,11 @@
-// import "extes";
-// import "/kernel/logger.esm.js";
-// import {ColorCode} from "/kernel/terminal-ctrl.esm.js";
 import fs from "fs";
-import { Serializer, Deserializer  } from "beson/beson.esm.js";
-import { Certificate } from "/lib/Certificate.esm.js";
+import { Beson, Serializer, Deserializer } from "beson/beson.esm.js";
+import { MEDIISCI } from "./lib/MEDIISCI.esm.js"
+
 
 (async () => {
 
-    const initialValue = 0;
-    const FMS =  Object.create(null);
     const write_Stream = Object.create(null);
-    // Object.defineProperty(FMS, 'state', {
-    //     get() { return initialValue; },
-    //     set(newValue) { initialValue = newValue; },
-    //     configurable: false,
-    //     enumerable: true,
-    // })
     Object.defineProperties(write_Stream, {
         'init': {
             value: () => { return Serializer.init(); },
@@ -24,26 +14,40 @@ import { Certificate } from "/lib/Certificate.esm.js";
             enumerable: true,
         },
         'write': {
-            value: serializer => {return serializer },
+            value: serializer => {
+                console.log('write_Stream ==>', serializer);                
+                return serializer 
+            },
             writable: true,
             configurable: false,
             enumerable: true,
         }
     })
 
-
+    const __Client = await MEDIISCI.Init(write_Stream);
+    console.log('Client Init: ', __Client.state );    
     
-    
+    const client_hello = __Client.Client_Hello(write_Stream);
+    console.log('Client Hello:', client_hello );
+
+    // let a = Deserializer.init(client_hello).read();
+    // console.log(a);
     
 
-    const { Client_Hello, Server_Hello, Server_Certificate_Request } = await import( '/lib/Hello.esm.js' );
-
-    const client = new Client_Hello(1, null, write_Stream)
-    console.log( 'client:', client );
+    const __Server = await MEDIISCI.Init(write_Stream);
+    const server_hello = __Server.Server_Hello(client_hello, write_Stream);
+    console.log('Server Hello:', server_hello );
     
-    console.log(write_Stream.write(5));
+    // const b = await __Server_Send_publicKey(a, write_Stream);
 
-    const server = new Server_Hello(1, )
+    // const { Client_Hello, Server_Hello, Server_Certificate_Request } = await import( '/lib/Hello.esm.js' );
+
+    // const client = new Client_Hello(1, null, write_Stream)
+    // console.log( 'client:', client );
+    
+    // console.log(write_Stream.write(5));
+
+    // const server = new Server_Hello(1, )
 
 })();
 
